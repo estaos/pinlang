8e78142c48c61dc6a716af8268e58c2a86a2cd4c
Batandwa Mgutsi <b.mgutsi@gmail.com>
2025-02-23 22:10:48 +0200

    Adds compiler hello world

---
+++ toolchain/compiler/bin/es_compiler.dart
@@ -0,0 +1,5 @@
+import 'package:es_compiler/es_compiler.dart' as es_compiler;
+
+void main(List<String> arguments) {
+  print('Hello world: ${es_compiler.calculate()}!');
+}
---
+++ toolchain/compiler/test/es_compiler_test.dart
@@ -0,0 +1,8 @@
+import 'package:es_compiler/es_compiler.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
