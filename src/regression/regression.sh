bf581b54c71a81706cd77644bb0d45fc153caef7
Batandwa Mgutsi <b.mgutsi@gmail.com>
2025-09-07 14:35:15 +0200

    Build on tag

--- src/regression/regression.sh
+++ src/regression/regression.sh
@@ -1,72 +1,85 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Building core..."
 mvn clean install --file ../core/pom.xml
+
+echo "Building transpiler..."
 mvn clean install --file ../transpiler/pom.xml
 
-# Imports
+# -------------------- Imports --------------------
+echo -e "\n\n===== Running Imports ====="
 cd ./regression/imports
-rm a.*
-rm file_*.h
-rm file_*.c
-mvn exec:java -Dexec.args="file_1.pin" --file ../../../transpiler/pom.xml
+rm -f a.*
+rm -f file_*.h
+rm -f file_*.c
+./../../../transpiler/target/pin "file_1.pin"
 clang main.c file_1.c file_2.c
 ./a
 cd ../../
 
-# Global variables
+# -------------------- Global Variables --------------------
+echo -e "\n\n===== Running Global Variables ====="
 cd ./regression/global_variables
-rm a.*
-rm *.h
-rm variables.c
-mvn exec:java -Dexec.args="variables.pin" --file ../../../transpiler/pom.xml
+rm -f a.*
+rm -f *.h
+rm -f variables.c
+./../../../transpiler/target/pin "variables.pin"
 clang main.c variables.c
 ./a
 cd ../../
 
-# Functions Hello Empty
+# -------------------- Functions Hello Empty --------------------
+echo -e "\n\n===== Running Functions Hello Empty ====="
 cd ./regression/functions
-rm a.*
-rm *.h
-rm *.c
-mvn exec:java -Dexec.args="hello_empty.pin" --file ../../../transpiler/pom.xml
+rm -f a.*
+rm -f *.h
+rm -f *.c
+./../../../transpiler/target/pin "hello_empty.pin"
 clang hello_empty.c
 ./a
 cd ../../
 
-# Functions Hello Empty With Return type
+# -------------------- Functions Hello Empty With Return Type --------------------
+echo -e "\n\n===== Running Functions Hello Empty With Return Type ====="
 cd ./regression/functions
-rm a.*
-rm *.h
-rm *.c
-mvn exec:java -Dexec.args="hello_empty_with_rtype.pin" --file ../../../transpiler/pom.xml
+rm -f a.*
+rm -f *.h
+rm -f *.c
+./../../../transpiler/target/pin "hello_empty_with_rtype.pin"
 clang hello_empty_with_rtype.c
 ./a
 cd ../../
 
-# Functions typedefs
+# -------------------- Functions Typedefs --------------------
+echo -e "\n\n===== Running Functions Typedefs ====="
 cd ./regression/functions
-rm a.*
-rm *.h
-rm *.c
-mvn exec:java -Dexec.args="typedef.pin" --file ../../../transpiler/pom.xml
+rm -f a.*
+rm -f *.h
+rm -f *.c
+./../../../transpiler/target/pin "typedef.pin"
 clang typedef.c
 ./a
 cd ../../
 
-# Functions hello world
+# -------------------- Functions Hello World --------------------
+echo -e "\n\n===== Running Functions Hello World ====="
 cd ./regression/functions
-rm a.*
-rm *.h
-rm *.c
-mvn exec:java -Dexec.args="hello_world.pin" --file ../../../transpiler/pom.xml
+rm -f a.*
+rm -f *.h
+rm -f *.c
+./../../../transpiler/target/pin "hello_world.pin"
 clang hello_world.c
 ./a
 cd ../../
 
-# Functions scan input
+# -------------------- Functions Scan Input --------------------
+echo -e "\n\n===== Running Functions Scan Input ====="
 cd ./regression/functions
-rm a.*
-rm *.h
-rm *.c
-mvn exec:java -Dexec.args="scan_input.pin" --file ../../../transpiler/pom.xml
+rm -f a.*
+rm -f *.h
+rm -f *.c
+./../../../transpiler/target/pin "scan_input.pin"
 clang scan_input.c
 ./a <<EOF
 James Bond
@@ -74,23 +87,24 @@ James Bond
 EOF
 cd ../../
 
-# Functions first class function
+# -------------------- Functions First Class Function --------------------
+echo -e "\n\n===== Running Functions First Class Function ====="
 cd ./regression/functions
-rm a.*
-rm *.h
-rm *.c
-mvn exec:java -Dexec.args="first_class_functions.pin" --file ../../../transpiler/pom.xml
+rm -f a.*
+rm -f *.h
+rm -f *.c
+./../../../transpiler/target/pin "first_class_functions.pin"
 clang first_class_functions.c
 ./a
 cd ../../
 
-# Control flow
+# -------------------- Control Flow --------------------
+echo -e "\n\n===== Running Control Flow ====="
 cd ./regression/control_flow
-rm a.*
-rm *.h
-rm *.c
-mvn exec:java -Dexec.args="main.pin" --file ../../../transpiler/pom.xml
+rm -f a.*
+rm -f *.h
+rm -f *.c
+./../../../transpiler/target/pin "main.pin"
 clang main.c
 ./a
-echo $?
 cd ../../
